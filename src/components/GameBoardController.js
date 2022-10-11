5865d2e2099052c26a77f87c61eac31f31e159e7
Tristen Everett <spock214@gmail.com>
2022-10-11 15:27:14 -0700

    refactor from function component into class component

--- src/components/GameBoardController.js
+++ src/components/GameBoardController.js
@@ -2,24 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 import CharacterDisplay from "./CharacterDisplay";
 
-function GameBoardController(props){
-  //Temp Variables
-  let map = props.map.map;
-  const mapSize = props.map.size;
-  let playerPosition = [0, 0];
-  let playerStats = props.character;
-  let enemyStats = null;
-  let battleMode = false;
-  for(let a = 0; a < map.length; a++){
-    for(let b = 0; b < map[a].length; b++){
-      if(map[a][b] === "U"){
-        playerPosition = [a, b];
-      }
-    }
+class GameBoardController extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      map: props.map.map,
+      mapSize: props.map.size,
+      playerPosition: [0, 0],
+      battleMode: false,
+      playerStats: props.character,
+      enemyStats: null
+    };
   }
 
-  //Style Logic
-  const styles = {
+  componentDidMount(){
+    let startingPosition = this.state.playerPosition
+    for(let a = 0; a < this.state.map.length; a++){
+      for(let b = 0; b < this.state.map[a].length; b++){
+        if(this.state.map[a][b] === "U"){
+          startingPosition = [a, b];
+        }
+      }
+    }
+    this.setState({playerPosition: startingPosition});
+  }
+
+  //Styles Object
+  styles = {
     arrows: {
       up: {
         height: 0,
@@ -79,112 +88,117 @@ function GameBoardController(props){
       }
     }
   };
-  function tablePosition(columnNumber, rowNumber){
-    const resultString = rowNumber + " / " + columnNumber;
-    return({gridArea: resultString});
-  }
 
   //Combat Logic
 
   //Movement Logic
-  function checkMovePossible(direction){
+  checkMovePossible(direction){
     let result = false;
     let newLocation = [];
     const directionCleansed = direction.toLowerCase();
     if(directionCleansed === "up"){
-      if(playerPosition[0] !== 0){
-        newLocation = [(playerPosition[0] - 1), (playerPosition[1])];
-        if(map[newLocation[0]][newLocation[1]] !== "X"){
+      if(this.state.playerPosition[0] !== 0){
+        newLocation = [(this.state.playerPosition[0] - 1), (this.state.playerPosition[1])];
+        if(this.state.map[newLocation[0]][newLocation[1]] !== "X"){
           result = true;
         }
       }
     }else if(directionCleansed === "down"){
-      if(playerPosition[0] !== (mapSize - 1)){
-        newLocation = [(playerPosition[0] + 1), (playerPosition[1])];
-        if(map[newLocation[0]][newLocation[1]] !== "X"){
+      if(this.state.playerPosition[0] !== (this.state.mapSize - 1)){
+        newLocation = [(this.state.playerPosition[0] + 1), (this.state.playerPosition[1])];
+        if(this.state.map[newLocation[0]][newLocation[1]] !== "X"){
           result = true;
         }
       }
     }else if(directionCleansed === "left"){
-      if(playerPosition[1] !== 0){
-        newLocation = [(playerPosition[0]), (playerPosition[1] - 1)];
-        if(map[newLocation[0]][newLocation[1]] !== "X"){
+      if(this.state.playerPosition[1] !== 0){
+        newLocation = [(this.state.playerPosition[0]), (this.state.playerPosition[1] - 1)];
+        if(this.state.map[newLocation[0]][newLocation[1]] !== "X"){
           result = true;
         }
       }
     }else if(directionCleansed === "right"){
-      if(playerPosition[1] !== (mapSize - 1)){
-        newLocation = [(playerPosition[0]), (playerPosition[1] + 1)];
-        if(map[newLocation[0]][newLocation[1]] !== "X"){
+      if(this.state.playerPosition[1] !== (this.state.mapSize - 1)){
+        newLocation = [(this.state.playerPosition[0]), (this.state.playerPosition[1] + 1)];
+        if(this.state.map[newLocation[0]][newLocation[1]] !== "X"){
           result = true;
         }
       }
     }
     return result;
   }
-  function movePlayer(direction){
+  movePlayer(direction){
     const directionCleansed = direction.toLowerCase();
     console.log("player moved " + directionCleansed);
   }
 
 
   //Render Function Logic
-  function buildArrow(direction, columnNumber, rowNumber){
+  buildArrow(direction, stylePosition){
     const directionCleansed = direction.toLowerCase();
-    if(checkMovePossible(directionCleansed)){
+    if(this.checkMovePossible(directionCleansed)){
       if(directionCleansed === "up"){
-        return(<div className="centered" style={tablePosition(columnNumber, rowNumber)} onClick={() => movePlayer("up")}><div style={styles.arrows.up}></div></div>);
+        return(<div className="centered" style={stylePosition} onClick={() => this.movePlayer("up")}><div style={this.styles.arrows.up}></div></div>);
       }else if(directionCleansed === "down"){
-        return(<div className="centered" style={tablePosition(columnNumber, rowNumber)} onClick={() => movePlayer("down")}><div style={styles.arrows.down}></div></div>);
+        return(<div className="centered" style={stylePosition} onClick={() => this.movePlayer("down")}><div style={this.styles.arrows.down}></div></div>);
       }else if(directionCleansed === "left"){
-        return(<div className="centered" style={tablePosition(columnNumber, rowNumber)} onClick={() => movePlayer("left")}><div style={styles.arrows.left}></div></div>);
+        return(<div className="centered" style={stylePosition} onClick={() => this.movePlayer("left")}><div style={this.styles.arrows.left}></div></div>);
       }else if(directionCleansed === "right"){
-        return(<div className="centered" style={tablePosition(columnNumber, rowNumber)} onClick={() => movePlayer("right")}><div style={styles.arrows.right}></div></div>);
+        return(<div className="centered" style={stylePosition} onClick={() => this.movePlayer("right")}><div style={this.styles.arrows.right}></div></div>);
       }
     }
   }
-  function healthRender(render, character){
+  healthRender(render, character){
     if(render){
       return(<CharacterDisplay simple={true} character={character} />);
     }
   }
 
-  //Return Logic
-  return(
-    <React.Fragment>
-      <div style={styles.tables.vertical}>
-        <div style={{...tablePosition(1, 1), ...styles.tables.top}}>
-          <div style={tablePosition(2, 1)}>
-            {healthRender(true, playerStats)}
+
+  render(){
+    //Style Logic
+    function tablePosition(columnNumber, rowNumber) {
+      const resultString = rowNumber + " / " + columnNumber;
+      return ({ gridArea: resultString });
+    }
+    
+    //Return Logic
+    return(
+      <React.Fragment>
+        <div style={this.styles.tables.vertical}>
+          <div style={{...tablePosition(1, 1), ...this.styles.tables.top}}>
+            <div style={tablePosition(2, 1)}>
+              {this.healthRender(true, this.state.playerStats)}
+            </div>
+            <div style={tablePosition(4, 1)}>
+              {this.healthRender(this.state.battleMode, this.state.enemyStats)}
+            </div>
           </div>
-          <div style={tablePosition(4, 1)}>
-            {healthRender(battleMode, enemyStats)}
+          <div style={{...tablePosition(1, 3), ...this.styles.tables.middle}}>
+            <div style={tablePosition(2, 1)}>
+              <p>Map</p>
+            </div>
+          </div>
+          <div style={{...tablePosition(1, 5), ...this.styles.tables.bottom}}>
+            <div style={tablePosition(2, 1)}>
+              <p>Attacks</p>
+            </div>
+            <div style={{...tablePosition(4, 1), ...this.styles.tables.movement}}>
+              {this.buildArrow("up", tablePosition(3, 2))}
+              {this.buildArrow("left", tablePosition(2, 3))}
+              {this.buildArrow("right", tablePosition(4, 3))}
+              {this.buildArrow("down", tablePosition(3, 4))}
+            </div>
           </div>
         </div>
-        <div style={{...tablePosition(1, 3), ...styles.tables.middle}}>
-          <div style={tablePosition(2, 1)}>
-            <p>Map</p>
-          </div>
-        </div>
-        <div style={{...tablePosition(1, 5), ...styles.tables.bottom}}>
-          <div style={tablePosition(2, 1)}>
-            <p>Attacks</p>
-          </div>
-          <div style={{...tablePosition(4, 1), ...styles.tables.movement}}>
-            {buildArrow("up", 3, 2)}
-            {buildArrow("left", 2, 3)}
-            {buildArrow("right", 4, 3)}
-            {buildArrow("down", 3, 4)}
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 GameBoardController.propTypes = {
   enemyList: PropTypes.array,
-  map: PropTypes.array,
+  map: PropTypes.object,
   character: PropTypes.object
 }
 
